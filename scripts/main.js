394b59535277426b7ef3e54b053b5b66fb49cf27
Mason <machreed@ucsc.edu>
2016-05-11 16:31:30 -0700

    Change movement to wasd

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,7 @@
-
-var KEYCODE_UP = 38;		//useful keycode
-var KEYCODE_LEFT = 37;		//useful keycode
-var KEYCODE_RIGHT = 39;		//useful keycode
-var KEYCODE_DOWN = 40;
+var KEYCODE_W = 87;
+var KEYCODE_A = 65;
+var KEYCODE_S = 83;
+var KEYCODE_D = 68;
 
 var keys = new Array();
 
@@ -34,7 +33,7 @@ function tick(event) {
 function moveCircle(){
 	var pt = circle.localToGlobal(0, 0);
 	
-	if(keys[KEYCODE_LEFT] === true && circle.x>0){
+	if(keys[KEYCODE_A] === true && circle.x>0){
 		if(pt.x > 200 || bmp.x >= 0){
 			circle.x -= 10;
 		}else{
@@ -42,7 +41,7 @@ function moveCircle(){
 		}
 	}
 	
-	if(keys[KEYCODE_RIGHT] === true && circle.x<stage.canvas.width-100){
+	if(keys[KEYCODE_D] === true && circle.x<stage.canvas.width-100){
 		if(pt.x < 500 || bmp.x+1600 <= 0){
 			circle.x += 10;
 		}else{
@@ -50,11 +49,11 @@ function moveCircle(){
 		}
 	}
 	
-	if(keys[KEYCODE_UP] === true && circle.y>0){
+	if(keys[KEYCODE_W] === true && circle.y>0){
 		circle.y -= 10;
 	}
 	
-	if(keys[KEYCODE_DOWN] === true && circle.y<stage.canvas.height-100){
+	if(keys[KEYCODE_S] === true && circle.y<stage.canvas.height-100){
 		circle.y += 10;
 	}
 	
