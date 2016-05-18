65c9d77f5171996f82be5b9ec606e185fa29e5a3
Nicholas Warren <ncwarren@ucsc.edu>
2016-05-17 18:01:28 -0700

    working on clue class
    Added placeholder assets for 3 different clues
    
    created Clue class and 3 instances of Clue class (clue1, clue2, clue3)
    
    picking up the clues does NOT work yet

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var keys = new Array();
 
 var stage, circle, rect, bmp, cop, clue, petey, inventory;
+var points;
 function init() {
 	stage = new createjs.Stage("demoCanvas"); //stage object 
 	var image = new Image();				 // image object for background 
@@ -12,6 +13,7 @@ function init() {
 	bmp.x = -800;
 	bmp.y = 0;
 	
+	//Adding Clue
 	var graphics = new createjs.Graphics().beginFill("Red").drawRect(0, 0, 10, 10);
 	clue = new createjs.Shape(graphics);
 	clue.x = 0;
@@ -19,6 +21,17 @@ function init() {
  	stage.addChild(clue);
  	clue.addEventListener("click", handleClick_clue);
  	
+
+ 	clue1 = new Clue(200, 200, 1, "./assets/clue1.png", 24,24);
+ 	stage.addChild(clue1);
+ 	clue1.addEventListener("click", handleClick_clue);
+ 	
+ 	clue2 = new Clue(300, 200, 1, "./assets/clue2.png", 24,24);
+ 	stage.addChild(clue2);
+ 	clue3 = new Clue(400, 200, 1, "./assets/clue3.png", 24,24);
+ 	stage.addChild(clue3);
+ 	
+ 	//Adding Player
 	var playerData = new createjs.SpriteSheet({		// investigator/player 
 		images: ["./assets/Character.png"],			// image source for inverstigator
 		frames: {width: 40, height: 135, count: 2},
@@ -30,10 +43,11 @@ function init() {
 	player = new createjs.Sprite(playerData);	
 	player.x = 400;
 	player.y = 235;
+	stage.addChild(player);
 	
+	//Adding Cop (NPC object instance)
 	cop = new NPC(100, 235, 1, "./assets/Copper.png", 35, 135);
 	stage.addChild(cop);
-	
 	cop.addEventListener("click", handleClick_cop);
 	function handleClick_cop(event) {
 	    console.log("cop clicked.");
@@ -42,21 +56,11 @@ function init() {
 	    }
 	 }
 	
-	petey = new NPC(100, 235, 1, "./assers/pety.png", 213, 112);
-	
-	
-	// I'm trying to display petey's image, not sure why its not working?? 
-	var peteyData = new createjs.SpriteSheet({		//petey sprite
-		images: ["./assets/petey.png"],				// image 
-		frames: {width: 213, height: 112, count:1},
-	});
-	petey = new createjs.Sprite(peteyData);		
-	petey.x = 100;
-	petey.y = 235;
-	
+	//Adding Petey (NPC object instance)
+	petey = new NPC(100, 235, 1, "./assets/petey.png", 213, 112);
 	stage.addChild(petey);
-	stage.addChild(player);
 	
+	//Create ticket (game loop)
 	createjs.Ticker.on("tick", game_loop);
 }
 
@@ -71,13 +75,14 @@ function update(){
 	createInventory();
 	move(player);
 	stickToBackground(cop, cop.back.x, cop.back.y);	// draw the sprites on the screen. 
-	stickToBackground(clue, 1300, 500);
-
+	stickToBackground(clue1, 1300, 500);
+	stickToBackground(clue2, 1400, 500);
+	stickToBackground(clue3, 1500, 500);
 	stickToBackground(petey, 2000, 450);
 	
 	//Display background mouse position in console
 	var mouse = getBackgroundPosition(stage.mouseX, stage.mouseY);
-	console.log("x: " + mouse.x + " y: " + mouse.y);
+	//console.log("x: " + mouse.x + " y: " + mouse.y);
 
 }
 
@@ -103,9 +108,12 @@ function getBackgroundPosition(x, y){
 }
 		
 function handleClick_clue(event){
+	console.log("clicked on clue");
 	var dist = getDistance(player.x, player.y, clue.x, clue.y);
 	if(dist < 135){
-		stage.removeChild(clue);
+		console.log("yes");
+		stage.removeChild(clue1);
+		points ++;
 	}
 }	
 
--- scripts/objects.js
+++ scripts/objects.js
@@ -1,3 +1,4 @@
+//NPC Object definition
 (function(){
 	function NPC(x, y, numframes, url, width, height){
 		this.spriteSheet = this.makeSheet(url, numframes, width, height);
@@ -18,4 +19,27 @@
 	};
 	
 	window.NPC = createjs.promote(NPC, "Sprite");
+}());
+
+//Clue Object definition
+(function(){
+	function Clue(x, y, numframes, url, width, height){
+		this.spriteSheet = this.makeSheet(url, numframes, width, height);
+		this.Sprite_constructor(this.spriteSheet);
+		
+		this.x = x;
+		this.y = y;
+		
+		this.back = getBackgroundPosition(x, y);
+	}
+	var p = createjs.extend(Clue, createjs.Sprite);
+	
+	p.makeSheet = function(url, numframes, width, height){
+		return new createjs.SpriteSheet({
+			images: [url],
+			frames: {width: width, height: height, count: numframes}
+		});
+	};
+	
+	window.Clue = createjs.promote(Clue, "Sprite");
 }());
