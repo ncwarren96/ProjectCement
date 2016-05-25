6db91fbcdf077aeb5a77f90f0761e716f1bc8b0e
Nicholas Warren <ncwarren@ucsc.edu>
2016-05-25 15:39:53 -0700

    fix?

--- scripts/main.js
+++ scripts/main.js
@@ -39,12 +39,17 @@ function init() {
 	
 	//Adding clues
  	clue1 = new Clue(500, 500, 1, "./assets/wallet.png", 24,24, "wallet");
+ 	clue1.clueInfo = makeInfoSprite("./assets/clue_info_sprite.png", 400, 300);
  	stage.addChild(clue1);
+ 	
  	clue2 = new Clue(700, 500, 1, "./assets/photo.png", 24,24, "photo");
+ 	clue2.clueInfo = makeInfoSprite("./assets/clue_info_sprite.png", 400, 300);
  	stage.addChild(clue2);
+ 	
  	clue3 = new Clue(600, 500, 1, "./assets/drugs.png", 24,24, "drugs");
+ 	clue3.clueInfo = makeInfoSprite("./assets/clue_info_sprite.png", 400, 300);
  	stage.addChild(clue3);
-	//stage.addChild(clue1.clueInfo);
+ 	
 
 	//Adding Cop (NPC object instance)
 	cop = new NPC(100, 235, 1, "./assets/Copper.png", 35, 135, "cop_beach");
