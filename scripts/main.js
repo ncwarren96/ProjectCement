aee3f4e0816e43cc3edbfeb0455930c8f0d7651e
Michael Schmidt <mjschmid@ucsc.edu>
2016-05-26 17:58:51 -0700

    changes made to clues
    changes made

--- scripts/main.js
+++ scripts/main.js
@@ -75,16 +75,16 @@ function init() {
 	player = new Player(400, 235, 2, "./assets/Character.png", 40, 135, "player");
 	
 	//Adding clues
- 	clue1 = new Clue(500, 500, 1, "./assets/wallet.png", 24,24, "wallet");
- 	clue1.clueInfo = makeInfoSprite("./assets/clue_info_sprite.png");
+ 	clue1 = new Clue(900, 450, 1, "./assets/wallet.png", 24,24, "wallet");
+ 	clue1.clueInfo = makeInfoSprite("./assets/wallet_Image.png");
  	beachMap.map_Objects.push(clue1);
  	
- 	clue2 = new Clue(700, 500, 1, "./assets/photo.png", 24,24, "photo");
- 	clue2.clueInfo = makeInfoSprite("./assets/clue_info_sprite.png");
+ 	clue2 = new Clue(1150, 500, 1, "./assets/ID_sprite.png", 24,24, "photo");
+ 	clue2.clueInfo = makeInfoSprite("./assets/ID.png");
  	beachMap.map_Objects.push(clue2);
  	
- 	clue3 = new Clue(600, 500, 1, "./assets/drugs.png", 24,24, "drugs");
- 	clue3.clueInfo = makeInfoSprite("./assets/clue_info_sprite.png");
+ 	clue3 = new Clue(700, 500, 1, "./assets/drugs.png", 24,24, "drugs");
+ 	clue3.clueInfo = makeInfoSprite("./assets/Drugs_image.png");
  	beachMap.map_Objects.push(clue3);
 
  	
