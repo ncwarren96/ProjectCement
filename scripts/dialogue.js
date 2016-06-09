ab75dc31886c35921d36442237d7b8bf293d563c
Nicholas Warren <ncwarren@ucsc.edu>
2016-06-09 15:57:08 -0700

    working on end dialogue, and added a file cabinet

--- scripts/dialogue.js
+++ scripts/dialogue.js
@@ -26,105 +26,6 @@ function showDialogue(targetDialogue, state){
 		}
 	}
 	
-	if(targetDialogue == "cop_beach"){
-		if(state == 0){
-			
-			createDialougeBox(textContainer, "Cop: A private investigator huh? We have everything under control here, you can go home.", 1, 2, 3, "Did you know Petey?", "Need help?", "I do what I want.");
-			
-		}else if(state == 1){
-			
-			createDialougeBox(textContainer, "Cop: I've heard the others mention him, he was top member in the 100 Cloccs, an off-shoot of the G-Stone Crips, so a pretty dangerous guy.", null, null, null, null, null, "Thanks.");
-			
-		}else if(state == 2){
-			
-			createDialougeBox(textContainer, "Cop: I already said we don't need help. Just go home.", null, null, null, "Ok, bye.", "I'll help anyways.", "I do what I want.");
-						
-		}else if(state == 3){
-			
-			createDialougeBox(textContainer, "Cop: Just don't mess anything up and mind your own business.", null, null, null, "*Leave*", "*Leave*", "*Leave*");		
-		}
-	}
-	
-	if(targetDialogue == "guy2"){
-		if(state == 0){
-			
-			createDialougeBox(textContainer, "Who the hell are you?", 1, 1, 1, "Ok, bye.", "Ok, bye.", "Ok, bye.");		
-						
-		}else if(state == 1){
-			
-			createDialougeBox(textContainer, "Bye.", null, null, null, "*Leave*", "*Leave*", "*Leave*");
-			
-		}
-	}
-	//dead petey 'Dialogue'
-	if(targetDialogue == "petey"){
-		peteyClue.handleClick();
-		textContainer.removeAllChildren();
-		boyce.questions = true;
-		console.log(boyce.questions);
-	}
-	
-	// guy 1 Dialogue
-	if(targetDialogue == "guy1"){
-		if(state == 0){
-			
-			createDialougeBox(textContainer, "Hey whats going on here? You work with the police?", 2, 1, 1, "respond", "ignore", "Ok, bye.");	
-			
-		}else if(state == 1){
-			
-			createDialougeBox(textContainer, "Bye.", null, null, null, "Leave", "Leave", "Leave");
-			
-		}else if(state == 2){
-			
-			createDialougeBox(textContainer, "No, im not with the police, im a private investigator", 3, 1, 1, "ask question", "ignore", "Leave");
-			
-		}else if(state == 3 ){
-			
-			createDialougeBox(textContainer, "Do you know who petey gonzales is ? ", 4, 1, 1, "listen", "Leave", "Leave"); 
-			
-		}else if(state == 4 ){
-			
-			createDialougeBox(textContainer, "Know idea who that is?  ", 5, 5, 5, "Leave", "Leave", "Leave");
-							
-		}else if(state == 5 ){
-			
-			createDialougeBox(textContainer, "Thank you for your time", null, null, null, "Leave", "Leave", "Leave");
-			
-		}
-	}
-	
-	//Creepy cult info man
-	if(targetDialogue == "creep"){
-		
-		var secret = secrets.pop();
-		
-		if(secret == null){
-			if(state == 0){
-				createDialougeBox(textContainer, "Go away", null, null, null, "Leave", null, null);
-			}else if(state == 1){
-				createDialougeBox(textContainer, "If you find any more of these clues, bring them to me.", null, null, null, "Ok...", null, null);
-				removeFromInventory(secretClue1);
-			}
-		}else{
-			
-			if(secret.label == "M"){
-				createDialougeBox(textContainer, "Animals don't eat fingers.", null, null, 1, null, null, "What?");
-			}else if(secret.label == "A"){
-				createDialougeBox(textContainer, "Petey owed a lot more than money. And to someone very important.", null, null, null, "To who?", "", "");
-				removeFromInventory(secretClue2);
-			}else if(secret.label == "N"){
-				createDialougeBox(textContainer, "The mob stopped dealing in drugs long ago.", null, null, null, null, null, "Leave");
-				removeFromInventory(secretClue3);
-			}else if(secret.label == "D"){
-				createDialougeBox(textContainer, "Mando controls this city, and everyone in it. But if you complete this case, we could stop them", null, null, null, null, null, "Leave");
-				removeFromInventory(secretClue4);
-			}else if(secret.label == "O"){
-				createDialougeBox(textContainer, "Mando Imperium knows.", null, null, null, "Leave", "Leave", "Leave");
-				removeFromInventory(secretClue5);
-			}
-		}	
-	}
-
 	//Boyce Dialouge
 	if(targetDialogue == "boyce"){
 		if(state == 0){
@@ -149,18 +50,78 @@ function showDialogue(targetDialogue, state){
 			
 		}else if(state == 10){
 			
-			createDialougeBox(textContainer, "Boyd: We've been trying to get him locked up for good, looks like someone else got him though.", 3, null, null, "I have another question.", "Leave", null);
+			createDialougeBox(textContainer, "Boyd: We've been trying to get him locked up for good, looks like someone else got him though.", 3, null, null, "I have another question.", "Leave", "null");
 			
 		}
 	}
 	
+	//Second beach cop dialogue
+	if(targetDialogue == "cop_beach"){
+		if(state == 0){
+			createDialougeBox(textContainer, "Cop: A private investigator huh? We have everything under control here, you can go home.", 1, 2, 3, "Did you know Petey?", "Need help?", "I do what I want.");
+		}else if(state == 1){		
+			createDialougeBox(textContainer, "Cop: I've heard the others mention him, he was top member in the 100 Cloccs, an off-shoot of the G-Stone Crips, so a pretty dangerous guy.", null, null, null, null, null, "Thanks.");			
+		}else if(state == 2){
+			createDialougeBox(textContainer, "Cop: I already said we don't need help. Just go home.", null, null, null, "Ok, bye.", "I'll help anyways.", "I do what I want.");	
+		}else if(state == 3){
+			createDialougeBox(textContainer, "Cop: Just don't mess anything up and mind your own business.", null, null, null, null, null, "*Leave*");		
+		}
+	}
+	
+	//Dog lady dialogue
+	if(targetDialogue == "guy2"){
+		if(state == 0){
+			createDialougeBox(textContainer, "Please don't pet my dog.", null, null, null, "Ok, bye.", "Whatever.", "I didn't want to anyways.");		
+		}
+	}
+	
+	//dead petey 'Dialogue'
+	if(targetDialogue == "petey"){
+		peteyClue.handleClick();
+		textContainer.removeAllChildren();
+		boyce.questions = true;
+		console.log(boyce.questions);
+	}
+	
+	//Creepy cult info man. Gives different responce depending on secret clue
+	if(targetDialogue == "creep"){
+		
+		var secret = secrets.pop();
+		
+		if(secret == null){
+			if(state == 0){
+				createDialougeBox(textContainer, "Go away.", null, null, null, "Leave", null, null);
+			}else if(state == 1){
+				createDialougeBox(textContainer, "If you find any more of these symbols, bring them to me. You can call me Chayce.", null, null, null, "Ok...", null, null);
+				removeFromInventory(secretClue1);
+			}
+		}else{
+			if(secret.label == "M"){
+				createDialougeBox(textContainer, "Animals don't eat fingers.", null, null, 1, null, null, "What?");
+			}else if(secret.label == "A"){
+				createDialougeBox(textContainer, "Chayce: Petey owed a lot more than money. And to someone very important.", null, null, null, "To who?", null, null);
+				removeFromInventory(secretClue2);
+			}else if(secret.label == "N"){
+				createDialougeBox(textContainer, "Chayce: The mob stopped dealing in drugs long ago.", null, null, null, null, null, "So someone else is...");
+				removeFromInventory(secretClue3);
+			}else if(secret.label == "D"){
+				createDialougeBox(textContainer, "Chayce: Mando controls this city, and everyone in it. But if you complete this case, we could stop them", null, null, null, null, null, "Leave");
+				removeFromInventory(secretClue4);
+			}else if(secret.label == "O"){
+				createDialougeBox(textContainer, "Chayce: Mando Imperium knows.", null, null, null, "Leave", "Leave", "Leave");
+				removeFromInventory(secretClue5);
+			}
+		}	
+	}
+
+	//phone booth/ Kristina dialogue
 	if(targetDialogue == "phone"){
 		if(state == 0){
 			createDialougeBox(textContainer, "Number?", 1, 9, 9, "555-1762", "555-9572", "555-0080");
 		}else if(state == 1){
 			createDialougeBox(textContainer, "Kristina: Hello?", 2, null, null, "Hello this is Private Investigator Clive Dixon.", "Sorry wrong number.", null);
 		}else if(state == 2){
-			createDialougeBox(textContainer, "Kristina: Did you find Petey?", 3, 4, 5, "Um... I'm working on it.", "Yes, he's in a lot of trouble'", "Yes. He's dead Kristina.");
+			createDialougeBox(textContainer, "Kristina: Did you find Petey?", 3, 4, 5, "Um... I'm working on it.", "Yes, he's in a lot of trouble.", "Yes. He's dead Kristina.");
 		
 		}else if(state == 3){
 			createDialougeBox(textContainer, "Kristina: Well what do you want?", 8, 7, 6, "Where did you say you last saw Petey?", "How long have you two known each other?", "Know any problems Petey was facing?");
@@ -187,12 +148,13 @@ function showDialogue(targetDialogue, state){
 		}
 	}
 	
+	//Vinny the barber dialogue
 	if(targetDialogue == "barber"){
 		console.log(talkedToKristina);
 		if(state == 0){
 			createDialougeBox(textContainer, "Need a new trim?", 2, talkedToKristina? 1 : 6, 5, "No thanks. But has the NYPD spoken to you lately?",  talkedToKristina? "Not today. Know the name Peter Gonzalez?" : "Not today" , "Sure.");
 		}else if(state == 1){
-			createDialougeBox(textContainer, "Dont know, doesn't ring a bell", 7, 6, 6, "Could you try looking him up in your customer files?", "Leave", "Leave");
+			createDialougeBox(textContainer, "Dont know, doesn't ring a bell", 7, 6, 6, "Could you try looking him up in your receipts?", "Leave", "Leave");
 		}else if(state == 2){
 			createDialougeBox(textContainer, "No, what would they need to know from me?", 4, 6, 6, "They said you murdered someone!", "Leave", "Leave");
 		}else if(state == 3){
@@ -206,17 +168,26 @@ function showDialogue(targetDialogue, state){
 		}else if(state == 7){
 			createDialougeBox(textContainer, "Sure, let me see...", 8, null, null, "...", "Leave", "Leave");
 		}else if(state == 8){
-			createDialougeBox(textContainer, "Here he is, was in here 2 weeks ago. I remember him now, weird fellow. Kept mumbling about a Mandolin? I assumed it was drugs."+
-											 " Then some guy came in here and said he had to go before I could finish.", null, null, 6, null, null, "Thanks.");
+			createDialougeBox(textContainer, "Here he is, was in here 2 weeks ago. I remember him now, weird fellow. Kept mumbling about a Mandolin? I assumed it was drugs.", 9, null, null, "-->", null, null);						 
+
+		}else if(state == 9){
+			createDialougeBox(textContainer, "Then some guy came in here and said he had to go before I could finish.", null, null, 6, null, null, "Thanks.");
 			barberDeathEvent = true;
-			endGameScenario = true;
+			
 		}
 	}
 	
-	if(targetDialogue == "barberDead"){
-		textContainer.removeAllChildren();
-		player.immobile = false;
+	//Man in haircut place
+	if(targetDialogue == "haircut"){
+		createDialougeBox(textContainer, "I'm waiting for a haircut.", null, null, null, "Leave", "Leave", "Leave");
 	}
+	
+	//Vinny's dead body
+	if(targetDialogue == "barberDead"){
+		createDialougeBox(textContainer, "A gunshot through the head...", null, null, null, null, null, "*Leave*");
+	}
+	
+	//City trash can with secret clue 4
 	if(targetDialogue == "trashCan"){
 		if(state==0){
 			createDialougeBox(textContainer, "*You dig this slip of paper from the garbage. Gross.*", null, null, null, null, null, "Leave");
@@ -225,18 +196,18 @@ function showDialogue(targetDialogue, state){
 		}
 	}
 	
+	//NYPD file cabinet with secret clue 5
 	if(targetDialogue == "trashCanNYPD"){
 		if(state==0){
-			createDialougeBox(textContainer, "*You managed to quickly grab this paper.*", null, null, null, null, null, "Leave");
-			//player.immobile = false;
+			createDialougeBox(textContainer, "Get out of there! That's confindential!", null, null, 1, null, null, "Sorry.");			
+		}else if(state==1){
+			createDialougeBox(textContainer, "*You managed to quickly grab this slip of paper.*", null, null, null, null, null, "*Leave*");
 			secretClue5.handleClick();
+			endGameScenario = true;
 		}
 	}
-	
-	if(targetDialogue == "haircut"){
-		createDialougeBox(textContainer, "I'm waiting for a haircut.", null, null, null, "Leave", "Leave", "Leave");
-	}
-	
+
+	//burning beach barrel 
 	if(targetDialogue == "barrel"){
 		if(state==0){
 			createDialougeBox(textContainer, "*You pull this slip of paper from the ashes*", null, null, null, null, null, "Leave");
@@ -245,11 +216,12 @@ function showDialogue(targetDialogue, state){
 		}
 	}
 	
+	//NYPD office front desk.
 	if(targetDialogue == "deskCop"){
 		if(state == 0){
-			createDialougeBox(textContainer, "Can I help You?", 2, 1, 1, "I need criminal files on peter gonzales", "ignore", "Ok, bye.");	
+			createDialougeBox(textContainer, "Can I help You?", 1, 2, 3, "I need you files on Peter Gonalez.", "Can I see Chief Boyd's files?", "Mando Imperium?");	
 		}else if(state == 1){
-			createDialougeBox(textContainer, "Bye.", null, null, null, "Leave", "Leave", "Leave");
+			createDialougeBox(textContainer, "We don't just give people criminal files.", null, null, null, "Leave", "Leave", "Leave");
 		}else if(state == 2){
 			createDialougeBox(textContainer, "we dont give out that kind of information", 3, 4, 1, "ask question", "SHOW ME THOSE FILES!", "Leave");
 		}else if(state == 3 ){
@@ -268,7 +240,7 @@ function showDialogue(targetDialogue, state){
 
 	if(targetDialogue == "cop_city2"){
 		if(state == 0){
-			createDialougeBox(textContainer, "GG WP?", 1, 1, 1, "GG", "GG", "GG");
+			createDialougeBox(textContainer, "Stop right there Dixon. You are under arrest for the murder of Vinny Aldready.", 1, 1, 1, "What?", "You can't be serious.", "GG");
 		}else if(state == 1){
 			loadMap("city2", "endStatsMap", "./assets/Background1.png", "./assets/Background1-hitbox.png");
 		}
